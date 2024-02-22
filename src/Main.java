3e417bead96e153c4bb5c07e92bb05741ea56f8e
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-02-22 18:36:55 +0300

    initial repository

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+
+// create MathPackage
+// make up the UI for actions with math operations include linear programming
+
+// remake structure creation methods in matrix and vector class like methods that calls constructor

5fc6b16ebac6704b96ca28a0197b40b96d129df2
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-03-08 00:53:53 +0300

    add another main method from second Main branch: ConsoleTests

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.lang.reflect.*;
+import java.util.*;
+import java.io.*;
 public class Main {
-    public static void main(String[] args) {
-        Matrix matrix = Matrix.createNewRandomIntMatrix(-99, 99);
-        matrix.printMatrix();
+    public static void main(String[] args) throws IOException {
+        String expressionText = "1 + x";
+        System.out.print("enter x = ");
+        Double x = Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        List<Parser.Lexeme> lexemes = Parser.lexAnalyze(expressionText);
+        Parser.LexemeBuffer lexemeBuffer = new Parser.LexemeBuffer(lexemes);
+        System.out.println(Parser.analyseExpression(lexemeBuffer));
+
+    }
+    public static class ConsoleTests {
+        public static void main(String[] args) throws IOException {
+            System.out.print("enter x = ");
+            Double x = Double.parseDouble(new BufferedReader(new InputStreamReader(System.in)).readLine());
+            System.out.println(x);
+        }
     }
 }
 

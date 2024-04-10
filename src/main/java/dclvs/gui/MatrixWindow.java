0f320388099a78d2d2ebf175fe1078469ea7150b
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-11 00:59:35 +0300

    another change package location

--- src/main/java/dclvs/gui/MatrixWindow.java
+++ src/main/java/dclvs/gui/MatrixWindow.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
 
 import java.util.List;
 
-import static dclvs.gui.gui.MainWindow.MainMenu;
+import static dclvs.gui.MainWindow.MainMenu;
 
 public class MatrixWindow {
     public static StackPane CombineWindow(Node... nodes) {

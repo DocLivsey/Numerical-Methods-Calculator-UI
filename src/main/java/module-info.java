dbd22a10e93b6f4cd36b72f52a2295cc2185b7d0
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-11 00:54:28 +0300

    add test hello app javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module dclvs.gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens dclvs.gui to javafx.fxml;
+    exports dclvs.gui;
+}

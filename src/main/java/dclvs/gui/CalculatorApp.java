3bba09789a5c8cd3df2f5f29f0686c3850942c21
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-17 16:04:44 +0300

    change stage setup

--- src/main/java/dclvs/gui/CalculatorApp.java
+++ src/main/java/dclvs/gui/CalculatorApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
@@ -22,12 +23,19 @@ public class CalculatorApp extends Application {
     protected Scene previousScene;
     @Override
     public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), Integer.MAX_VALUE, Integer.MAX_VALUE);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public void testWindow(Stage primaryStage) throws IOException {
         this.primaryStage = primaryStage;
         MainWindow.setMainWindow(this);
         this.primaryStage.setTitle("Calculator");
         this.primaryStage.show();
-    }
-    public static void main(String[] args) {
-        launch();
     }
 }

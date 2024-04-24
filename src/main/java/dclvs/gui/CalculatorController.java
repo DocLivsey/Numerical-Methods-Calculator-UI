3efb10da7cfe2a952b93523e60e4be1ffdb7a9de
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-24 17:21:23 +0300

    add new methods in controller class for new app for change scenes

--- src/main/java/dclvs/gui/CalculatorController.java
+++ src/main/java/dclvs/gui/CalculatorController.java
@@ -1,4 +1,49 @@
 package dclvs.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TitledPane;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
 public class CalculatorController {
+
+    @FXML
+    private ToggleGroup MatricesCalc;
+
+    @FXML
+    private RadioButton eigenProblemRadio;
+
+    @FXML
+    private RadioButton matricesOperationsRadio;
+
+    @FXML
+    private TitledPane matricesTiledPane;
+
+    public void switchMatricesOperationsScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass()
+                .getResource("Matrices/operationsScene.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchEigenProblemScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass()
+                .getResource("Matrices/eigenProblemScene.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }

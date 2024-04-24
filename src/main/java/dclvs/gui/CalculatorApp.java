3255cd5a142bf044f532df5ee53e7e30ea28b005
DocLivsey <113336575+DocLivsey@users.noreply.github.com>
2024-04-24 17:20:31 +0300

    remove old gui realisation methods and change fxml file loc reference

--- src/main/java/dclvs/gui/CalculatorApp.java
+++ src/main/java/dclvs/gui/CalculatorApp.java
@@ -1,41 +1,24 @@
 package dclvs.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.*;
 
 import java.io.*;
-import java.util.List;
 
 
 public class CalculatorApp extends Application {
-    protected Stage primaryStage;
-    protected Scene currentScene;
-    protected Scene previousScene;
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("calculator-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorApp.class.getResource("welcomeWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), Integer.MAX_VALUE, Integer.MAX_VALUE);
+        primaryStage.setTitle("Numerical Methods Calculator");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
     public static void main(String[] args) {
         launch();
-    }
-
-    public void testWindow(Stage primaryStage) throws IOException {
-        this.primaryStage = primaryStage;
-        MainWindow.setMainWindow(this);
-        this.primaryStage.setTitle("Calculator");
-        this.primaryStage.show();
     }
 }
